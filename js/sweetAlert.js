8fdb3e57973db07f3d9de97e677277202741082d
krismarjfa <maestrakrismarpreescolar@gmail.com>
2022-02-02 03:13:31 -0400

    Mi portafolio

---
+++ js/sweetAlert.js
@@ -0,0 +1,22 @@
+// Alert
+$("#alert1").click(function(){
+    Swal.fire({
+  imageUrl: 'img/mobile.gif',
+  imageWidth: 220,
+  imageHeight:200,
+  title: 'Desarrollo Móvil',
+  text:'*Resposive                                              *Responsive',
+  width: 600,
+  padding: '3em',
+  color: '#00000',
+  background: '#fff ',
+//  backdrop: `
+//     rgba(0,0,123,0.4)
+//     url("img/estrellas.gif")
+//     left top
+//     no-repeat
+    
+//   `
+  
+})
+})
